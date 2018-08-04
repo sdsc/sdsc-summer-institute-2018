f9bc1caa9208f766ddfe4f3a54dc1d18dd7057aa
Andrea Zonca <zonca@sdsc.edu>
2018-08-04 14:12:36 -0700

    use only 6 workers

--- hpc0_python_hpc/digits_of_pi/3_dask_multicore_digits_of_pi.py
+++ hpc0_python_hpc/digits_of_pi/3_dask_multicore_digits_of_pi.py
@@ -2,15 +2,16 @@
 import sys
 import numpy as np
 import dask.array as da
+import dask
 
 def inside_circle(total_count):
 
-    x = da.random.uniform(size=total_count, chunks=total_count//48)
-    y = da.random.uniform(size=total_count, chunks=total_count//48)
+    x = da.random.uniform(size=total_count, chunks=total_count//6)
+    y = da.random.uniform(size=total_count, chunks=total_count//6)
 
     radii_square = x**2 + y**2
 
-    count = (radii_square<=1.0).sum().compute()
+    count = (radii_square<=1.0).sum()
 
     return count
 
@@ -19,12 +20,11 @@ def estimate_pi(n_samples):
     return (4.0 * inside_circle(n_samples) / n_samples)
 
 if __name__=='__main__':
-
     n_samples = 10000
     if len(sys.argv) > 1:
         n_samples = int(sys.argv[1])
 
-    my_pi = estimate_pi(n_samples)
+    my_pi = estimate_pi(n_samples).compute(num_workers=6)
     sizeof = np.dtype(np.float64).itemsize
 
     print("pi is {} from {} samples".format(my_pi,n_samples))
