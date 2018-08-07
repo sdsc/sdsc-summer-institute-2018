f413c35e722c64a0454cde768c3289121dd733db
Andrea Zonca <zonca@sdsc.edu>
2018-08-06 19:41:18 -0700

    use public ip

--- hpc0_python_hpc/launch_scheduler.sh
+++ hpc0_python_hpc/launch_scheduler.sh
@@ -2,4 +2,4 @@ echo "Launching dask scheduler"
 # fix for ubuntu container
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-dask-scheduler --scheduler-file ~/.dask_scheduler.json
+dask-scheduler --scheduler-file ~/.dask_scheduler.json --host $(hostname)
